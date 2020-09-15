662b036bb821b9367c2d0043bcdd72934f529251
Isabella Pegoraro Macondes <isabella.pegorarom@gmail.com>
2020-09-15 17:36:20 -0300

    Finalizando a lógica e adicionando responsividade

--- torre-de-hanoi.js
+++ torre-de-hanoi.js
@@ -3,10 +3,39 @@ let torre2Arr = [];
 let torre3Arr = [];
 let aux = [];
 let counter = 1;
+let minutesLabel = document.getElementById("minutes");
+let secondsLabel = document.getElementById("seconds");
+let totalSeconds = 0;
+
+function timer() {
+
+  let intervalo = setInterval(setTime, 1000);
+
+  function stop(){
+    clearInterval(intervalo)
+  }
+
+  function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  }
+
+  function pad(val) {
+    let valString = val + "";
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
+  }
+
+}
+
+document.getElementById('btStart').addEventListener('click', inicialize)
 
 
-
-function createInitialTower(){
+function createInitialTower() {
 
   let torre1 = document.getElementById("torre1");
   let torre2 = document.getElementById("torre2");
@@ -33,11 +62,11 @@ function createInitialTower(){
 
   let middleDisc = document.createElement("div");
   middleDisc.id = "middle";
-  torre1.appendChild(middleDisc); 
+  torre1.appendChild(middleDisc);
 
   let bottomDisc = document.createElement("div");
   bottomDisc.id = "bottom";
-  torre1.appendChild(bottomDisc); 
+  torre1.appendChild(bottomDisc);
 
   let divCounter = document.getElementById("statsContainer");
   let counterSpan = document.createElement("span");
@@ -46,101 +75,119 @@ function createInitialTower(){
 }
 createInitialTower();
 
-function moveDisc(arrAux, arr2, clickedTower){
-  if (arrAux.length === 0){
-    takeDisc(arr2);
-  } else {
-    putDisc(arr2, clickedTower);
-  }
-}
+let restartDis = document.getElementById("btRestart");
+restartDis.setAttribute("disabled", "true");
+restartDis.classList.add('disabled')
 
-function takeDisc(arr){
-  let removedDisc = arr.shift();
-  aux.push(removedDisc);
 
-  document.getElementById(`${removedDisc}`).remove();
-}
+function inicialize() {
 
-function putDisc(arr, clickedTower){ 
-  document.getElementById("jogadasNum").textContent = counter++; 
+  let modoJogador = document.getElementById('modoJogador')
+  modoJogador.innerText = "Pegue um disco para muda-lo de lugar"
 
-  arr.unshift(aux[0]); 
+  timer()
+  let disable = document.getElementById("btStart");
+  disable.setAttribute("disabled", "true");
+  disable.classList.add('disabled')
 
-  let col = document.getElementById(`col${clickedTower}`);
-  let newDisc = document.createElement("div");
-  newDisc.id = `${aux[0]}`; 
-  col.after(newDisc);
+  restartDis.removeAttribute("disabled")
+  restartDis.classList.remove('disabled')
 
-  aux = [];
 
-  let width0 = newDisc.clientWidth; 
-  let width1 = document.getElementById(`${clickedTower}`).lastElementChild.clientWidth; 
-  
-  // condição de derrota
-  if (width0 > width1){
-   
+  function moveDisc(arrAux, arr2, clickedTower) {
+    if (arrAux.length === 0) {
+      modoJogador.innerText = "Agora, escolha uma torre para coloca-lo"
+      takeDisc(arr2);
+    } else {
+      putDisc(arr2, clickedTower);
+    }
   }
 
-  // condição de vitória
-  if (clickedTower === "torre3" && arr.length === 3){
-    
+  function takeDisc(arr) {
+
+    let removedDisc = arr.shift();
+    aux.push(removedDisc);
+
+    document.getElementById(`${removedDisc}`).remove();
+
+    let disco = document.getElementById('disco')
+    let discDiv = document.createElement('div')
+    discDiv.id = aux[0]
+    disco.appendChild(discDiv)
   }
-}
 
-let tower1_function = document.getElementById("torre1").onclick = function(){
+  function putDisc(arr, clickedTower) {
 
-    if (aux.length !== 0 || torre1Arr.length !== 0){
-      moveDisc(aux, torre1Arr, "torre1"); 
+
+    document.getElementById("jogadasNum").textContent = counter++;
+
+    arr.unshift(aux[0]);
+
+    let col = document.getElementById(`col${clickedTower}`);
+    let newDisc = document.createElement("div");
+    newDisc.id = `${aux[0]}`;
+    col.after(newDisc);
+    document.getElementById(`${aux[0]}`).remove()
+    aux = [];
+    modoJogador.innerText = "Pegue um disco para muda-lo de lugar"
+
+
+    let width0 = newDisc.clientWidth;
+    let width1 = document.getElementById(`${clickedTower}`).lastElementChild.clientWidth;
+
+    // condição de derrota
+    if (width0 > width1) {
+      modoJogador.innerText = ""
+      let result = document.getElementById('resultado')
+      result.innerText = "Game Over!"
+      result.style = "color: darkred"
+
     }
 
-}
+    // condição de vitória
+    if (clickedTower === "torre3" && arr.length === 3) {
+      modoJogador.innerText = ""
 
-let tower2_function = document.getElementById("torre2").onclick = function(){
+      let result = document.getElementById('resultado')
+      result.innerText = "Parabéns!"
+      result.style = "color: darkgreen"
 
-    if (aux.length !== 0 || torre2Arr.length !== 0){
+    }
+  }
+
+  let tower1_function = document.getElementById("torre1").onclick = function () {
+
+    if (aux.length !== 0 || torre1Arr.length !== 0) {
+      moveDisc(aux, torre1Arr, "torre1");
+    }
+
+  }
+
+  let tower2_function = document.getElementById("torre2").onclick = function () {
+
+    if (aux.length !== 0 || torre2Arr.length !== 0) {
       moveDisc(aux, torre2Arr, "torre2");
     }
 
-}
-
-let tower3_function = document.getElementById("torre3").onclick = function(){
-
-    if (aux.length !== 0 || torre3Arr.length !== 0){
-      moveDisc(aux, torre3Arr, "torre3"); 
-    }
-
-}
-
-function timer(){
-  var minutesLabel = document.getElementById("minutes");
-  var secondsLabel = document.getElementById("seconds");
-  var totalSeconds = 0;
-  
-  setInterval(setTime, 1000);
-  
-  function setTime() {
-    ++totalSeconds;
-    secondsLabel.innerHTML = pad(totalSeconds % 60);
-    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
   }
 
-  function pad(val) {
-    var valString = val + "";
-    if (valString.length < 2) {
-      return "0" + valString;
-    } else {
-      return valString;
+  let tower3_function = document.getElementById("torre3").onclick = function () {
+
+    if (aux.length !== 0 || torre3Arr.length !== 0) {
+      moveDisc(aux, torre3Arr, "torre3");
     }
+
   }
 
-  document.getElementById("btStart").setAttribute("disabled", "true");
+
+
+  function restart() {
+    location.reload();
+  }
+
+  document.getElementById("btRestart").addEventListener("click", restart);
 }
 
-function restart(){
-  location.reload();
-}
-
-document.getElementById("btRestart").addEventListener("click", restart); 
 
 
 
@@ -158,83 +205,4 @@ document.getElementById("btRestart").addEventListener("click", restart);
 
 
 
-
-
-
-
-
-
-
-
-
-
-// function firstClick(){
-
-//   document.getElementById(torre1[0]).style = "display: none";
-//   let retirado = torre1.shift();
-//   aux.push(retirado);
-
-//   return retirado;
-// }
-
-// let retirado = firstClick(); 
-
-
-// let torre1 = document.getElementById('torre1').id
-// console.log(torre1)
-
-// //Pega todas as divs
-// const cells = document.querySelectorAll('div')
-
-// //pega o id
-// const handleClick = function(event) {
-//     const cell = event.target
-//     let a = cell.id
-//     console.log(a);
-//    // sumir(cell.id)
-// }
-// const handleClick = function(event) {
-//     const cell = event.target
-//     console.log(a);
-//    // sumir(cell.id)
-// }
-
-// //itera nas divs e coloca um event listener de click
-// for (let i = 0; i < cells.length; i++) {
-//     cells[i].addEventListener('click', handleClick);
-// }
-
-// //some com as divs
-// function sumir(id){
-//     document.getElementById(id).style = "display: none"
-//     aparecer(id)
-// }
-
-// function aparecer(id){
-//     document.getElementById(id).classList.remove('.colunaTop')
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function currentDiv(){
-
-//   let current = event.currentTarget;
-
-//   console.log(current);
-
-
-// }
-
-// let btCurrent = document.getElementById("conteiner");
-// btCurrent.addEventListener("click", currentDiv); 
 
